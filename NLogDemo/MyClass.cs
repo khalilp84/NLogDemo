bda3092baf48866096d100c5df84814f4b1c8232
Leandro Khalil <leandro.khalil84@gmail.com>
2015-11-19 14:56:39 -0200

    First commit NLog solution example

---
+++ NLogDemo/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo
+{
+    public class MyClass
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void TestNLog()
+        {
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
+
+            // alternatively you can call the Log() method 
+            // and pass log level as the parameter.
+            logger.Log(LogLevel.Info, "Sample informational message");
+        }
+    }
+}
